16d9c3a3beec820aede4ea234df8aa6f01789c90
Offir Talmor <talmor@tonkean.com>
2017-06-10 15:14:45 +0300

    Implementing SMS Broadcast WIP

--- sms_broadcast.py
+++ sms_broadcast.py
@@ -3,20 +3,39 @@ import sys, getopt
 
 
 def main(argv):
-    try:
-        opts, args = getopt.getopt(argv, "hf:t:n:p:", ["file=", "template=", "name_column=", "phone_column="])
-    except getopt.GetoptError:
-        print('sms_broadcast.py -f <csv_file> -t <text_template> -n <name_column_name> -p <phone_number_column_name>')
-        sys.exit(2)
+    file, nameColumn, phoneColumn, template, sendReal = parseArgs(argv)
 
+    rows = pd.read_csv(file)
+    rows = rows[[nameColumn, phoneColumn]]
+
+    for index, row in rows.iterrows():
+        text = template.replace('<name>', row[nameColumn])
+        sendSms(row[phoneColumn], text, sendReal)
+
+
+def sendSms(phone, text, sendReal):
+    print('Sending SMS to {}. text: {}'.format(phone, text))
+
+    # if sendReal == True:
+        # Send SMS.
+
+def parseArgs(argv):
+    try:
+        opts, args = getopt.getopt(argv, "hf:t:n:p:s", ["file=", "template=", "name_column=", "phone_column=", "send_real"])
+    except getopt.GetoptError:
+        print('sms_broadcast.py -s -f <csv_file> -t <text_template> -n <name_column_name> -p <phone_number_column_name>')
+        sys.exit(2)
     file = ''
     template = ''
     nameColumn = 'Name'
     phoneColumn = 'Phone number'
+    sendReal = False
     for opt, arg in opts:
         if opt == '-h':
-            print('sms_broadcast.py -f <csv_file> -t <text_template> -n <name_column_name> -p <phone_number_column_name>')
+            print('sms_broadcast.py -s -f <csv_file> -t <text_template> -n <name_column_name> -p <phone_number_column_name>')
             sys.exit()
+        elif opt in ("-s", "--send_real"):
+            sendReal = True
         elif opt in ("-f", "--file"):
             file = arg
         elif opt in ("-t", "--template"):
@@ -25,15 +44,7 @@ def main(argv):
             nameColumn = arg
         elif opt in ("-p", "--phone_column"):
             phoneColumn = arg
-
-    sendSms(file, template, nameColumn, phoneColumn)
-
-
-def sendSms(file, template, nameColumn, phoneColumn):
-    rows = pd.read_csv(file)
-    rows = rows[[nameColumn, phoneColumn]]
-
-    print(rows)
+    return file, nameColumn, phoneColumn, template, sendReal
 
 
 if __name__ == "__main__":
